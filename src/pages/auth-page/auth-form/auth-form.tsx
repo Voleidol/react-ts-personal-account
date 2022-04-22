2b0c5d9d1134304fc12f5682965a1449e2283b1f
Роман Кривошеев <krivosheev@amz.lan>
2022-04-22 15:45:38 +0300

    add auth page

---
+++ src/pages/auth-page/auth-form/auth-form.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import "./auth-form.css";
+
+export const AuthForm = () => {
+  return (
+    <div className="auth-form">
+      <Typography
+        variant="h4"
+        component="div"
+      >
+        h1. Heading
+      </Typography>
+    </div>
+  );
+};
